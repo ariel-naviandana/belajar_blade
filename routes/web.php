2606788da05dfd23617f609802e185ef9c5ef017
Ariel2711 <arielnaviandanaputra@gmail.com>
2025-02-28 14:44:20 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/blog', [BlogController::class, 'index']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
